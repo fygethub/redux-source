57bf6edda104171044c1c91c0ea8a9cdf61bf67f
冯毅 <yi.feng01@weimob.com>
2020-10-09 10:33:10 +0800

    add: 添加index文件

---
+++ redux/index.js
@@ -0,0 +1,9 @@
+const applyMiddleware = require('./applyMiddleware');
+const combineReducers = require('./combineReducers');
+const creatStore = require('./creatStore');
+
+module.exports = {
+    applyMiddleware,
+    combineReducers,
+    creatStore
+};
